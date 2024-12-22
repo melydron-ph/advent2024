ec0ced183d54a821ecbaf73b119d1f60f553c3d3
Peter Hadjigeorgiou <petrosha@gmail.com>
2024-12-22 23:49:27 +0200

    day22 done

--- Days/Day22.cs
+++ Days/Day22.cs
@@ -8,6 +8,7 @@ using static advent2024.Days.Day13;
 using System.ComponentModel;
 using System.Reflection;
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace advent2024.Days
 {
@@ -21,7 +22,6 @@ namespace advent2024.Days
             File.WriteAllText(OutputFile, string.Empty);
             string[] lines = File.ReadAllLines(InputFile);
             int[] numbers = lines.Select(line => int.Parse(line.Trim())).ToArray();
-
             long result = SecretNumbersSum(numbers, 2000);
             stopwatch.Stop();
             Console.WriteLine($"22*1 -- {result} ({stopwatch.ElapsedMilliseconds} ms)");
@@ -31,20 +31,84 @@ namespace advent2024.Days
             var stopwatch = System.Diagnostics.Stopwatch.StartNew();
             File.WriteAllText(OutputFile, string.Empty);
             string[] lines = File.ReadAllLines(InputFile);
-            int result = 0;
+            int[] numbers = lines.Select(line => int.Parse(line.Trim())).ToArray();
+            long result = BananaStuff(numbers, 2000);
+
             stopwatch.Stop();
             Console.WriteLine($"22*2 -- {result} ({stopwatch.ElapsedMilliseconds} ms)");
         }
 
+        private static long BananaStuff(int[] numbers, long d)
+        {
+            //numbers[0] = 123;
+            _ = SecretNumbersSum(numbers, d);
+            var priceChanges = new Dictionary<(int number, int position), int>();
+            for (int k = 0; k < numbers.Length; k++)
+            {
+                int number = numbers[k];
+                int prevPrice = 0;
 
+                //string lookup = ("-2,1,-1,3");
+                //if (buyOptions.ContainsKey(lookup))
+                //{
+                //    int num = buyOptions[lookup];
+                //    int index = buyOptions.Keys.ToList().IndexOf(lookup);
+                //}
+                for (int i = 0; i < d; i++)
+                {
+                    int newPrice = (int)(secretNumbers[(number, i)] % 10);
+                    int priceChange = newPrice - prevPrice;
+                    priceChanges[(number, i)] = priceChange;
+                    prevPrice = newPrice;
+
+                    if (i >= 4)
+                    {
+                        var sequence = new List<int>();
+                        sequence.Add(priceChanges[(number, i - 3)]);
+                        sequence.Add(priceChanges[(number, i - 2)]);
+                        sequence.Add(priceChanges[(number, i - 1)]);
+                        sequence.Add(priceChanges[(number, i)]);
+                        string sequenceKey = string.Join(",", sequence);
+                        if (!sequenceContributors.ContainsKey(sequenceKey))
+                        {
+                            sequenceContributors[sequenceKey] = new HashSet<int>();
+                        }
+
+                        if (!sequenceContributors[sequenceKey].Contains(number))
+                        {
+                            //if (sequenceKey == "-2,1,-1,3")
+                            //    Console.WriteLine($"{sequenceKey} : {number} : {newPrice}");
+                            if (!buyOptions.ContainsKey(sequenceKey))
+                                buyOptions[sequenceKey] = newPrice;
+                            else
+                                buyOptions[sequenceKey] += newPrice;
+
+                            sequenceContributors[sequenceKey].Add(number);
+                        }
+
+                    }
+                }
+            }
+            //foreach (var kvp in buyOptions.OrderByDescending(x => x.Value))
+            //{
+            //    //Console.WriteLine($"Sequence {kvp.Key}: {kvp.Value}");
+            //}
+            var bestSequence = buyOptions.OrderByDescending(x => x.Value).First();
+            return bestSequence.Value;
+
+        }
+
+        private static Dictionary<string, HashSet<int>> sequenceContributors = new();
+        private static Dictionary<string, int> buyOptions = new();
         private static Dictionary<(long num, long d), long> secretNumbers = new();
         private static long SecretNumbersSum(int[] numbers, long d)
         {
             long result = 0;
             foreach (var number in numbers)
             {
+                secretNumbers[(number, 0)] = number;
                 long secNum = number;
-                var key = (secNum, d);
+                var key = (number, d);
                 if (secretNumbers.ContainsKey(key))
                 {
                     result += secretNumbers[key];
@@ -53,6 +117,7 @@ namespace advent2024.Days
                 for (long i = 1; i <= d; i++)
                 {
                     secNum = GetSecretNumber(secNum, i);
+                    secretNumbers[(number, i)] = secNum;
                 }
                 result += secNum;
             }
@@ -61,9 +126,6 @@ namespace advent2024.Days
 
         private static long GetSecretNumber(long num, long d)
         {
-            var key = (num, d);
-            if (secretNumbers.ContainsKey(key))
-                return secretNumbers[key];
 
             long secNum = num;
             secNum = Mix(secNum, 64 * secNum);
@@ -73,7 +135,6 @@ namespace advent2024.Days
             secNum = Mix(secNum, secNum * 2048);
             secNum = Prune(secNum);
 
-            secretNumbers[(num, d)] = secNum;
             return secNum;
         }
 
